38fa5100fcfd2ffe68f435f25272b06df9f87766
abid <chiheb.abid@gmail.com>
2026-08-20 11:54:56 +0100

    Add CMake configuration and initial application code for LVGL integration

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,25 @@
 //
 
 #include <lv/lv.hpp>
+#include <lv/assets/cursor.hpp>
 
 int main() {
     lv::init();
-    lv::X11Display display("Hello", 480, 320);
+    // Initiate SDL display
+    lv::SDLDisplay display(800, 600);
 
     lv::Label::create(lv::screen_active())
         .text("Hello, LVGL!")
         .center();
 
+    lv::text_button(lv::screen_active(), "Hello")
+        .text("Click Me")
+        .size(120, 50)
+        .center()
+        .on_click([](lv::Event) {
+            lv::Label::create(lv::screen_active())
+                .text("Button Clicked!")
+                .center();
+        });
     lv::run();
 }
